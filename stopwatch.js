c5ff4ac7dc2c28e57c9ce8bb21a0301d2676ff20
digvijayyanand <99537348+digvijayyanand@users.noreply.github.com>
2022-04-25 20:17:32 +0530

    basic changes
    logic remains the same

--- stopwatch.js
+++ stopwatch.js
@@ -28,30 +28,32 @@ function stop_timer() {
 
 function timer_func() {
   if (stop_timer_var == false) {
-    sec = parseInt(sec);//converting sec to int value i.e from string to int
-    min = parseInt(min);
-    hr = parseInt(hr);
     sec = sec + 1;
-    if (sec == 60) {//for every 60s one min is added and then sec is made zero
+    if (sec == 60) {
+      //for every 60s one min is added and then sec is made zero
       min = min + 1;
       sec = 0;
     }
-    if (min == 60) {//similar is the case for min and hours
+    if (min == 60) {
+      //similar is the case for min and hours
       hr = hr + 1;
       min = 0;
       sec = 0;
     }
-
-    if (sec < 10) {
-      sec = '0' + sec;//string
-    }
-    if (min < 10) {
-      min = '0' + min;
-    }
+    let timer_print='';
     if (hr < 10) {
-      hr = '0' + hr;
+      timer_print += '0';//string
     }
-    timer.innerHTML = hr + ': ' + min + ': ' + sec;
+    timer_print += hr + ': ';
+    if (min < 10) {
+      timer_print += '0';//string
+    }
+    timer_print+=min + ': ';
+    if (sec < 10) {
+      timer_print+= '0'//string
+    }
+    timer_print+=sec;
+    timer.innerHTML =timer_print;
     setTimeout("timer_func()", 1000);
   }
 }
