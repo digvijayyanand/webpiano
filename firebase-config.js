2e54b65c9007e69965153acf51ac75744f98f63a
digvijayyanand <99537348+digvijayyanand@users.noreply.github.com>
2022-05-03 21:19:58 +0530

    new features

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 var firebaseConfig={
-  apiKey: "AIzaSyDZbyIMsKkyzkR47pitjBZkavyTTBfsScY",
-  authDomain: "piano-towards-end.firebaseapp.com",
-  projectId: "piano-towards-end",
-  storageBucket: "piano-towards-end.appspot.com",
-  messagingSenderId: "171901730776",
-  appId: "1:171901730776:web:ceac2d7dd8da7a54bbfcad",
-  measurementId: "G-T65DQYYXEQ"
+  apiKey: "AIzaSyDrc1osmhp0xzrLOdeSD3ziCk_3oFtUmCI",
+  authDomain: "piano-project-find-users-also.firebaseapp.com",
+  projectId: "piano-project-find-users-also",
+  storageBucket: "piano-project-find-users-also.appspot.com",
+  messagingSenderId: "323422574541",
+  appId: "1:323422574541:web:c0a1d3c13e82643640d684",
+  measurementId: "G-8R7T54JQ4K"
 };
 var app=firebase.initializeApp(firebaseConfig);
 firebase.analytics();
